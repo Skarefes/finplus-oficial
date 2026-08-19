f08f92b9ce77eb43016fd2acb9f56d0f3b152209
Roger Yamassaki <skarefes@gmail.com>
2026-08-19 19:39:33 -0300

    Atualizando

--- Backend/src/main/java/com/yama/finplus/domain/cartao/ParcelaService.java
+++ Backend/src/main/java/com/yama/finplus/domain/cartao/ParcelaService.java
@@ -5,6 +5,7 @@ import com.yama.finplus.repository.ParcelaRepository;
 import org.springframework.stereotype.Service;
 
 import java.math.BigDecimal;
+import java.math.RoundingMode;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -17,15 +18,27 @@ public class ParcelaService {
     }
 
     public void gerarParcelas(Financeiro financeiro, Integer quantidadeParcelas) {
-        int quantidade =  (quantidadeParcelas == null)? 1:quantidadeParcelas;
+        int quantidade = (quantidadeParcelas == null) ? 1 : quantidadeParcelas;
 
         //calcular o valor de uma parcela
         List<Parcela> parcelas = new ArrayList<>();
         //pega o valor total do Financeiro e divide pela quantidade de parcelas para descobrir o valor de cada parcela.
-        BigDecimal valorParcela = financeiro.getValor().divide(BigDecimal.valueOf(quantidade));
+        //Iremos fazer com que seja limitado a 2 casas eusaremos o down porque queremos que a diferença fique para a última parcela
+        //roundingmode é um enum que define como um número deve ser arredondado quando uma operação matemática resulta em mais casas decimais, ou com mais precisao de valor
+        BigDecimal valorParcela = financeiro.getValor().divide(BigDecimal.valueOf(quantidade), 2, RoundingMode.DOWN);
+        //valor distribuido começa com zero
+        BigDecimal valorDistribuido = BigDecimal.ZERO;
         for (int i = 1; i <= quantidade; i++) {
-           Parcela parcela = new Parcela(financeiro, i, quantidade ,valorParcela);
-           parcelas.add(parcela);
+            BigDecimal valorAtual = valorParcela;
+
+            if (i == quantidade){
+                valorAtual = financeiro.getValor().subtract(valorDistribuido);
+            }
+
+            Parcela parcela = new Parcela(financeiro, i, quantidade, valorAtual);
+            parcelas.add(parcela);
+
+            valorDistribuido = valorDistribuido.add(valorAtual);
         }
 
         parcelaRepository.saveAll((parcelas));
